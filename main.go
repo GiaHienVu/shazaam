14a1a322dd219bb1db5846bee1ab6c8fda37aba2
hvjexxx@gmail.com <hvjexxx@gmail.com>
2026-03-09 17:49:17 +0700

    test

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"shazam/spotify"
+	"shazam/test"
 )
 
 func main() {
 	//fmt.Print(spotify.TrackInfo("https://open.spotify.com/track/14qNHvX8h6HoynFuV0RxWS?si=7583dc2959d741e6"))
-	fmt.Print(spotify.PlayListInfo2("https://open.spotify.com/albums/3zVOHT1NsTDeG4NyjZFlbA?si=xzbysKj3R0WY5gwKCUiXPQ"))
+	fmt.Print(test.PlayListInfo2("https://open.spotify.com/albums/3zVOHT1NsTDeG4NyjZFlbA?si=xzbysKj3R0WY5gwKCUiXPQ"))
 }
 
 // find(filePath) đã có filePATH MP3( GHI ÂM TỪ MIC ) -->Chuyển sang WAV --> Tạo finderprints --> Lưu vào DB
